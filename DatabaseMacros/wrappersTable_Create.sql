df2bf68daa374e028d1e3384f74454989b2f145c
Stephen Harold Smith <stephen@haroldsmith.org>
2023-07-04 12:44:24 +0100

    Not fully updated and some bits broken but there may be some malware so iM pushing..

--- DatabaseMacros/wrappersTable_Create.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO 'Wrappers' ('Name', 'Path', 'ScriptsPath', 'Size', 'Permissions', 'LineCount') 
-	VALUES ('utils', '/usr/local/bin', '/usr/local/bin/utild.d', '13B', 750, 'root', 'admin');
