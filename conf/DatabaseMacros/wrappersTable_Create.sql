43d316f6ac0d6fade945be81f668f28cfb05f3c7
Stephen Harold Smith <stephen@haroldsmith.org>
2023-08-01 09:11:44 +0100

    All directories

---
+++ conf/DatabaseMacros/wrappersTable_Create.sql
@@ -0,0 +1,2 @@
+INSERT INTO 'Wrappers' ('Name', 'Path', 'ScriptsPath', 'Size', 'Permissions', 'LineCount') 
+	VALUES ('utils', '/usr/local/bin', '/usr/local/bin/utild.d', '13B', 750, 'root', 'admin');
